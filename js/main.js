f63d8ce43ad7230dbbd167157fd485d8801ee1f0
Bulumko Papu <bulumkopapu@gmail.com>
2022-02-18 17:22:15 +0200

    v1.3 Clock App Build

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+const button = document.getElementsByClassName("button")[0];
+const buttonText = document.getElementById("button-text");
+const arrow = document.getElementsByClassName("arrow")[0];
+
+// const bgImage = document.getElementsByClassName("bg-image")[0];
+// bgImage.style.backgroundImage = "url(/images/bg-night.png)";
+
+button.addEventListener("click", ()=>{
+    buttonText.textContent.includes('MORE') ? buttonText.textContent = "LESS" : buttonText.textContent = "MORE";
+    arrow.classList.toggle("active") 
+})
+
+
+
 
